cb8d6b9a2a5ba8b4a92d931532e8b4fad3119475
marialonso@gmail.com <marialonso@gmai.com>
2014-02-14 12:02:54 -0600

    ver 1
    ver 1

--- js/acciones.js
+++ js/acciones.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function() {
 	document.addEventListener("deviceready", function(){
-		
+	   $('#B1').tap(function(){
+		    navigator.notification.beep(3);
+	   }); //TAP B1
 	}); //device ready
 });//ready
